bb2d0e09d654150286ca8aa16589f17e2289bc12
TAKUANf <takuan.tn@gmail.com>
2025-02-19 01:22:18 +0900

    Update

--- update.bat
+++ update.bat
@@ -9,7 +9,7 @@ cd /d "%REPO_DIR%"
 if errorlevel 1 (
     echo エラー: '%REPO_DIR%' ディレクトリに移動できませんでした。
     echo update.bat と同じディレクトリでこのスクリプトを実行していること、
-    echo およびリポジトリがクローンされていることを確認してください。
+    echo 及び、リポジトリがクローンされていることを確認してください。
     pause
     exit /b 1
 )
